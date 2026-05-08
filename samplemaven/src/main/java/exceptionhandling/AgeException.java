42f083d0073787f7ac5a08bca6e3c11c96ea63c2
Meera Raghavan <Meera Raghavan@Meenus>
2026-05-08 11:25:29 +0530

    8-05-26

--- samplemaven/src/main/java/exceptionhandling/AgeException.java
+++ samplemaven/src/main/java/exceptionhandling/AgeException.java
@@ -2,7 +2,7 @@ package exceptionhandling;
 
 public class AgeException extends Exception {
 
-	public AgeException(String string) {
+	public AgeException(String s) {
 	super(s);	// TODO Auto-generated constructor stub
 	}
 
